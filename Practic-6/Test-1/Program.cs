8ee4179eea83f1963effef68360799453e577ca3
Valdis <balakinvad@gmail.com>
2022-09-09 09:55:02 +0300

    Сделал практическое №6

---
+++ Practic-6/Test-1/Program.cs
@@ -0,0 +1,43 @@
+﻿//Вариант 1
+Console.Write("Введите числа(через пробел): ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Кол-во элементов > 0: {count}");
+
+//Вариант 2
+// Console.Write($"Введи число М(количество чисел): ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int[] numbers = new int[m];
+
+// void InputNumbers(int m)
+// {
+//     for (int i = 0; i < m; i++)
+//     {
+//         Console.Write($"Введи {i + 1} число: ");
+//         numbers[i] = Convert.ToInt32(Console.ReadLine());
+//     }
+// }
+
+// int Compar(int[] numbers)
+// {
+//     int count = 0;
+//     for (int i = 0; i < numbers.Length; i++)
+//     {
+//         if (numbers[i] > 0)
+//             count += 1;
+//     }
+//     return count;
+// }
+
+// InputNumbers(m);
+
+// Console.WriteLine($"Кол-во элементов > 0: {Compar(numbers)} ");
---
+++ Practic-6/Test-2/Program.cs
@@ -0,0 +1,46 @@
+﻿double[,] coefficient = new double[2, 2];
+double[] crossPoint = new double[2];
+
+void InputCoefficients()
+{
+    for (int i = 0; i < coefficient.GetLength(0); i++)
+    {
+        Console.Write($"Введите коэффициенты {i + 1}-го уравнения (y = k * x + b) ");
+        for (int j = 0; j < coefficient.GetLength(1); j++)
+        {
+            if (j == 0)
+                Console.Write($"Введите коэффициент k: ");
+            else
+                Console.Write($"Введите коэффициент b: ");
+            coefficient[i, j] = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
+
+double[] Decision(double[,] coefficient)
+{
+    crossPoint[0] =
+        (coefficient[1, 1] - coefficient[0, 1]) / (coefficient[0, 0] - coefficient[1, 0]);
+    crossPoint[1] = crossPoint[0] * coefficient[0, 0] + coefficient[0, 1];
+    return crossPoint;
+}
+
+void OutputResponse(double[,] coefficient)
+{
+    if (coefficient[0, 0] == coefficient[1, 0] && coefficient[0, 1] == coefficient[1, 1])
+    {
+        Console.Write($"Прямые совпадают");
+    }
+    else if (coefficient[0, 0] == coefficient[1, 0] && coefficient[0, 1] != coefficient[1, 1])
+    {
+        Console.Write($"Прямые параллельны");
+    }
+    else
+    {
+        Decision(coefficient);
+        Console.Write($"Точка пересечения прямых: ({crossPoint[0]}, {crossPoint[1]})");
+    }
+}
+
+InputCoefficients();
+OutputResponse(coefficient);
